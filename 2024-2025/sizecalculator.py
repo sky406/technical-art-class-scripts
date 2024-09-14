57a32733fafd62df02beedaa9cb8b710bbc55bb0
sky406 <60670435+sky406@users.noreply.github.com>
2024-09-14 13:34:53 -0700

    fixed scale not being checked when measuring

--- 2024-2025/sizecalculator.py
+++ 2024-2025/sizecalculator.py
@@ -6,16 +6,17 @@ def confirmprompt(message:str,buttons:str|list,title:str = "confirm"):
         t = title
     )
 
-selections = m.ls(sl=1)
-objects = {}
-for i in selections:
-    objects[i] = m.ls(f"{i}.vtx[*]",fl=1)
-print(objects)
-
-if selections == []:
-    confirmprompt("select something","ok","nothing selected")
-
 def measure():
+    selections = m.ls(sl=1)
+    objects = {}
+    for i in selections:
+        objects[i] = m.ls(f"{i}.vtx[*]",fl=1)
+    print(objects)
+
+    if selections == []:
+        confirmprompt("select something","ok","nothing selected")
+
+
     for i in objects.keys():
         vertices = objects[i]
         xyzmax = [0,0,0]
@@ -23,7 +24,7 @@ def measure():
 
         for vertex in vertices:
             position = m.pointPosition(vertex,l=1)
-            # print(position)
+            print(position)
             count = 0
             for coordinate in position:
                 if position[count]>xyzmax[count]:
@@ -34,13 +35,13 @@ def measure():
             
         print(f"xyz max = {xyzmax}  and xyz min = {xyzmin}")
 
-        xsize = xyzmax[0]-xyzmin[0]
-        ysize = xyzmax[1]-xyzmin[1]
-        zsize = xyzmax[2]-xyzmin[2]
+        xsize = xyzmax[0]-xyzmin[0] * m.getAttr(f"""{i}.scaleX""")
+        ysize = xyzmax[1]-xyzmin[1] * m.getAttr(f"""{i}.scaleY""")
+        zsize = xyzmax[2]-xyzmin[2] * m.getAttr(f"""{i}.scaleZ""")
 
         print (f"""{i}'s dimensions are 
-        \n height:{round(ysize,2)} 
-        \n width:{round(xsize,2)} 
-        \n depth:{round(zsize,2)}""")
+        \n height:{round(ysize,3)} 
+        \n width:{round(xsize,3)} 
+        \n depth:{round(zsize,3)}""")
 
 measure()
