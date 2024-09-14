779ce273979db1e063fb18e05080cb4da49fe878
sky406 <60670435+sky406@users.noreply.github.com>
2024-09-14 11:23:45 -0700

    Update getting point positions.py

--- 2024-2025/getting point positions.py
+++ 2024-2025/getting point positions.py
@@ -2,6 +2,5 @@ objects = m.ls(sl=1,fl=1)
 print(objects)
 m.getAttr("idk.translateX")
 for i in objects:
-    #m.getAttr(f"{i}")
     #this getsthe position of verticies 
     print(m.pointPosition(i,l=1))
