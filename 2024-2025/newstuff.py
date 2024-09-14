411e82dfaeeb70526bb8eba91a8e98eef3e6b015
sky406 <60670435+sky406@users.noreply.github.com>
2024-09-14 10:31:03 -0700

    more stuff

--- 2024-2025/newstuff.py
+++ 2024-2025/newstuff.py
@@ -1,5 +1,7 @@
 import maya.cmds as m
-import random as r
+from random import uniform as randf
+
 m.polyCube(h=6,w=6,d=6,n="idkman#")
-m.rotate(r.uniform(0,360.0),r.uniform(0,360.0),r.uniform(0,360.0))
-m.move(r.uniform(0,30.0),r.uniform(0,30.0),r.uniform(0,30.0))
+m.rotate(randf(0,360.0),randf(0,360.0),randf(0,360.0))
+m.move(randf(0,30.0),randf(0,30.0),randf(0,30.0))
+m.select(cl=1)
