3e06c9203d7caa8992ec6e95a158b8a040dc078e
sky406 <60670435+sky406@users.noreply.github.com>
2024-11-03 14:03:33 -0800

    Update Nworisa_Wesley_Scene_poulator.py

--- Nworisa_Wesley_Scene_poulator.py
+++ Nworisa_Wesley_Scene_poulator.py
@@ -10,23 +10,28 @@ def pivotMove(shape,position,height,width,depth):
     rotpiv = f"{shape[0]}.rotatePivot"
     def pivDown():
         m.move(0,-0.5*height,0,scalepiv,rotpiv,r=1)
-        print("moved")
-    def pivSide():
-        m.move(0.5*width,0,0,scalepiv,rotpiv,r=1)
-        print("moved side")
-    def pivCorner(side):
+    def pivCornerRadius(side):
         match side:    
             case "x":
                 m.move(width,-0.5*height,0,scalepiv,rotpiv)
             
             case "z":
                 m.move(0,-0.5*height,depth,scalepiv,rotpiv)
+    def pivCorner(side):
+        match side:    
+            case "x":
+                m.move(0.5*width,-0.5*height,0,scalepiv,rotpiv)
+            
+            case "z":
+                m.move(0,-0.5*height,-0.5*depth,scalepiv,rotpiv)
 
     match position:
         case "bottom":
             pivDown()
-        case "side":
-            pivSide()
+        case "cornerradX":
+            pivCornerRadius("x")
+        case "cornerradZ":
+            pivCornerRadius("z")
         case "cornerX":
             pivCorner("x")
         case "cornerZ":
@@ -38,7 +43,7 @@ def baseToCenter(shape,height):
 def coneToside(cone,height,radius):
     coneangle = mat.degrees(mat.atan(height/radius))
     rotangle = 180-coneangle
-    pivotMove(cone,"cornerX",height,radius,None)
+    pivotMove(cone,"cornerradX",height,radius,None)
     m.rotate(0,0,-rotangle,cone[0])
     resetRotation(cone)
     # baseToCenter(cone,height)
@@ -46,25 +51,28 @@ def coneToside(cone,height,radius):
 def resetRotation(shape):
     m.makeIdentity(shape[0],r=1,a=1)
 
-def rot90(shape,axis:str,height,width,depth):
-    match axis:
-        case "x":
-            pivotMove(shape,"cornerX",height,width,depth)
-            m.rotate(0,0,-90,shape[0])
-            
-        case "z":
-            pivotMove(shape,"cornerZ",height,width,depth)
-            m.rotate(-90,0,0,shape[0])
-            
+def rot90(shape,axis:str,height,width,depth,isCube):
+    if isCube:
+        match axis:
+            case "x":
+                pivotMove(shape,"cornerX",height,width,depth)
+                m.rotate(0,0,-90,shape[0])
+                
+            case "z":
+                pivotMove(shape,"cornerZ",height,width,depth)
+                m.rotate(-90,0,0,shape[0])
+    else:
+        match axis:
+            case "x":
+                pivotMove(shape,"cornerradX",height,width,depth)
+                m.rotate(0,0,-90,shape[0])
+                
+            case "z":
+                pivotMove(shape,"cornerradZ",height,width,depth)
+                m.rotate(-90,0,0,shape[0])
+                
     resetRotation(shape)
 
-def createArray(arrayrange:int):
-    array = []
-    for x in range(-arrayrange,arrayrange,1):
-        for y in range(-arrayrange,arrayrange,1):
-            array.append([x,y])
-    print(array)
-    return array
 
 def CreateBlacklistdict(shape):
     bbox = m.exactWorldBoundingBox(shape[0])
@@ -72,7 +80,6 @@ def CreateBlacklistdict(shape):
     zmin = bbox[2]
     xmax = bbox[3]
     zmax = bbox[5]
-    print(f"{xmax}<>{xmin} , {zmax}<>{zmin}")
     dict = {
         "xmin":xmin,
         "xmax":xmax,
@@ -81,46 +88,57 @@ def CreateBlacklistdict(shape):
     }
     return dict
 
-def moveFromCollision(shape,Bbox):
+
+def checkcollisions(shape,blacklist):
+    collisionFound = False
+    shapeBbox = CreateBlacklistdict(shape)
     def isbetween(target,lowerbound,upperbound):
         return target>=lowerbound and target<=upperbound
-    shapeBbox = CreateBlacklistdict(shape)
-    # variables for algnment with coordinates
-    xPos = isbetween(shapeBbox["xmax"],Bbox["xmin"],Bbox["xmin"])
-    xNeg = isbetween(shapeBbox["xmin"],Bbox["xmin"],Bbox["xmax"])
-    zPos = isbetween(shapeBbox["zmax"],Bbox["zmin"],Bbox["zmax"])
-    zNeg = isbetween(shapeBbox["zmin"],Bbox["zmin"],Bbox["zmax"])
-    if (xPos or xNeg) and (zPos or zNeg) :
-        moveX = 0
-        moveZ = 0
-        # i know it is confusing but i'e already commited to the poitive negative x, it caould be changed later if i have time 
-        posXdistance = shapeBbox["xmax"]-Bbox["xmin"]
-        negXdistance = Bbox["xmax"]-shapeBbox["xmin"]
+    for Bbox in blacklist:
+        xPos = isbetween(shapeBbox["xmax"],Bbox["xmin"],Bbox["xmin"])
+        xNeg = isbetween(shapeBbox["xmin"],Bbox["xmin"],Bbox["xmax"])
+        zPos = isbetween(shapeBbox["zmax"],Bbox["zmin"],Bbox["zmax"])
+        zNeg = isbetween(shapeBbox["zmin"],Bbox["zmin"],Bbox["zmax"])
+        if (xPos or xNeg) and (zPos or zNeg) :
+            collisionFound=True
+            print(f"collision detected {shape[0]}")
+            moveX = 0
+            moveZ = 0
+            # i know it is confusing but i've already commited to the poitive negative x, it caould be changed later if i have time 
+            posXdistance = shapeBbox["xmax"]-Bbox["xmin"]
+            negXdistance = Bbox["xmax"]-shapeBbox["xmin"]
 
-        posZdistance = shapeBbox["zmax"]-Bbox["zmin"]
-        negZdistance = Bbox["zmax"]-Bbox["zmin"]
-        if xPos and xNeg:    
-            if posXdistance < negXdistance:
+            posZdistance = shapeBbox["zmax"]-Bbox["zmin"]
+            negZdistance = Bbox["zmax"]-Bbox["zmin"]
+            if xPos and xNeg:    
+                if posXdistance < negXdistance:
+                    moveX = -posXdistance
+                else:
+                    moveX = negXdistance
+            elif xPos:
                 moveX = -posXdistance
             else:
                 moveX = negXdistance
-        elif xPos:
-            moveX = -posXdistance
-        else:
-            moveX = negXdistance
-        
-        if zPos and zNeg:    
-            if posXdistance < negZdistance:
+            
+            if zPos and zNeg:    
+                if posXdistance < negZdistance:
+                    moveZ = -posZdistance
+                else:
+                    moveZ = negZdistance
+            elif xPos:
                 moveZ = -posZdistance
             else:
                 moveZ = negZdistance
-        elif xPos:
-            moveZ = -posZdistance
-        else:
-            moveZ = negZdistance
+            m.move(moveX*2,0,moveZ*2,shape[0],r=1)
+    if collisionFound:
+        checkcollisions(shape,blacklist)
+    #probabbly not work working on maybe will reitterate later 
+    
         
 
+
 def createShapes(type:str,number:int,spreadRange,scaleRange,height,width,depth):
+    collisionBlacklist = []
     for i in range(number):
         match type:
             case "cube":
@@ -135,9 +153,9 @@ def createShapes(type:str,number:int,spreadRange,scaleRange,height,width,depth):
             if type =="cone":
                 coneToside(shape,height,width)
             elif type =="cube":
-                rot90(shape,choice(["x","z"]),height,width,depth)
+                rot90(shape,choice(["x","z"]),height,width,depth,True)
             else: 
-                rot90(shape,"x",height,width,depth)
+                rot90(shape,"x",height,width,depth,False)
         else:
             pivotMove(shape,"bottom",height,width,depth)
         baseToCenter(shape,height)
@@ -149,12 +167,19 @@ def createShapes(type:str,number:int,spreadRange,scaleRange,height,width,depth):
         m.scale(scale,scale,scale,shape[0])
         
         # scatter the shapes
-        collisionBlacklist = []
 
         def randomposition():
             return uniform(-spreadRange,spreadRange)
+        
         m.move(randomposition(),0,randomposition(),shape[0],r=1)
-        # collisionBlacklist = 
+        if collisionBlacklist == []:
+            collisionBlacklist.append(CreateBlacklistdict(shape))
+            print("first bbox added")
+        else:
+            m.xform(shape[0],cp=1) #center pivots
+            checkcollisions(shape,collisionBlacklist)
+            collisionBlacklist.append(CreateBlacklistdict(shape))
+        
         
 #test call please ignore           
-createShapes("cylinder",30,900,12,9,6,2)
+createShapes("cube",30,900,12,9,6,2)
